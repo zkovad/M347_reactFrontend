6134397ee00ef3b0b7fa1c5ea8c10ae20900159b
esilva <esilva@ethz.ch>
2024-05-17 16:13:40 +0200

    Copied Cavoti's Frontend

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./cartContext";
+import ErrorBoundary from "./ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ErrorBoundary>
+        <BrowserRouter>
+            <CartProvider>
+              <React.StrictMode>
+                <App />
+              </React.StrictMode>
+            </CartProvider>
+        </BrowserRouter>
+    </ErrorBoundary>,
 );
 
 // If you want to start measuring performance in your app, pass a function
